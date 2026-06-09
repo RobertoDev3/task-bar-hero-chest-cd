d83b8919ba06eb3bb9ac9c9814a283eabe27f675
RobertoDev3 <roberto.dev3@hotmail.com>
2026-06-09 20:35:25 -0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,38 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      'react/prop-types': 'off',
+      'import/no-duplicates': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/camelcase': 'off',
+      'import/no-anonymous-default-export': [
+        'warn',
+        { allowObject: true, allowArrowFunction: true },
+      ],
+      semi: ['error'],
+      quotes: ['warn', 'single', { avoidEscape: true }],
+      'prefer-arrow-callback': ['error'],
+      'prefer-template': ['error'],
+    },
+  }),
+];
 
 export default eslintConfig;
